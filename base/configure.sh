a0c941ad8fc64b4280913879855e13f270ad2904
Matthew Farrellee <matt@redhat.com>
2015-02-23 10:40:06 -0500

    add base image

---
+++ base/configure.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+cat >> conf/storm.yaml <<EOF
+storm.local.dir: "/tmp"
+EOF
+
+if [ -n "$1" ]; then
+   cat >> conf/storm.yaml <<EOF
+storm.zookeeper.servers:
+- "$1"
+EOF
+fi
+
+if [ -n "$2" ]; then
+   cat >> conf/storm.yaml <<EOF
+nimbus.host: "$2"
+EOF
+fi   
+
+cat conf/storm.yaml
